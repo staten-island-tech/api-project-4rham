1fc01a7490e9ea7be20b37c41f841c19c3df8bd9
4rham <95187191+4rham@users.noreply.github.com>
2023-01-06 10:15:46 -0500

    gvjvjhvhvjh

--- api-project/main.js
+++ api-project/main.js
@@ -1,3 +1,20 @@
 import "./style.css";
 
-URL;
+const URL = "https://api.nasa.gov/planetary/apod";
+
+async function getData(URL) {
+  try {
+    const response = await fetch(URL);
+    if (response.status < 200 || response.status > 299) {
+      throw new Error(await response);
+    } else {
+      const data = await response.json();
+      document.getElementById("api-response").textContent = data.content;
+      console.log("yay!");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("no!");
+  }
+}
+getData(URL);
